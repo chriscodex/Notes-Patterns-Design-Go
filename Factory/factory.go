b065f1d05d7f565d49b0b71dee044b55f39315c2
Christian <3003html@gmail.com>
2022-07-16 22:38:29 -0500

    main function created

--- Factory/factory.go
+++ Factory/factory.go
@@ -74,3 +74,7 @@ func getComputerFactory(computerType string) (iProduct, error) {
 func printNameAndStock(p iProduct) {
 	fmt.Printf("Product Name:%s, with stock:%d\n", p.getName(), p.getStock())
 }
+
+func main() {
+
+}
