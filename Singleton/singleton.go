5d6ee6304dc649f5760fc5e35506c99407df31c6
Christian <3003html@gmail.com>
2022-07-18 02:17:09 -0500

    Second way to create singleton done

--- Singleton/singleton.go
+++ Singleton/singleton.go
@@ -18,7 +18,7 @@ func (database) createSingleConnection() {
 var db *database
 var mutex sync.Mutex
 
-func getDataBaseInstance() *database {
+func getDataBaseInstance1() *database {
 	mutex.Lock()
 	defer mutex.Unlock()
 	if db == nil {
@@ -31,13 +31,30 @@ func getDataBaseInstance() *database {
 	return db
 }
 
+// Other way
+var once sync.Once
+
+func getDataBaseInstance2() *database {
+	once.Do(func() {
+		fmt.Println("Creating DB Connection")
+		if db == nil {
+			fmt.Println("Creating DB Connection")
+			db = &database{}
+			db.createSingleConnection()
+		} else {
+			fmt.Println("DB already created")
+		}
+	})
+	return db
+}
+
 func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			getDataBaseInstance()
+			getDataBaseInstance2()
 		}()
 	}
 	wg.Wait()
