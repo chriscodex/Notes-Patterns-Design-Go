2b1943bc0483999f828b66ae9975a9ec4edc3d7b
Christian <3003html@gmail.com>
2022-07-19 07:59:25 -0500

    constructor of newEmailCLient created

--- Observer/observer.go
+++ Observer/observer.go
@@ -25,8 +25,8 @@ func newItem(name string) *item {
 }
 
 func (i *item) updateAvailable() {
-	fmt.Printf("Item %s is available\n", i.name)
 	i.available = true
+	fmt.Printf("Item %s is available\n", i.name)
 	i.broadcast()
 }
 
@@ -45,22 +45,24 @@ type emailClient struct {
 	id string
 }
 
+func newEmailClient(id string) *emailClient {
+	return &emailClient{
+		id: id,
+	}
+}
+
 func (eC *emailClient) getId() string {
 	return eC.id
 }
 
-func (eC *emailClient) updateValue(value string) {
-	fmt.Printf("Sending email -%s available for client %s\n", value, eC.id)
+func (eC *emailClient) updateValue(item string) {
+	fmt.Printf("Sending email -%s available for client %s\n", item, eC.id)
 }
 
 func main() {
 	nvidiaItem := newItem("RTX 3080")
-	firstObserver := &emailClient{
-		id: "12ab",
-	}
-	secondObserver := &emailClient{
-		id: "34dc",
-	}
+	firstObserver := newEmailClient("12ab")
+	secondObserver := newEmailClient("34bc")
 	nvidiaItem.register(firstObserver)
 	nvidiaItem.register(secondObserver)
 	nvidiaItem.updateAvailable()
