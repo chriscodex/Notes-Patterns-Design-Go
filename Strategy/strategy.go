f6eebd103eea9cf417f5a5f86db0680e780e719d
Christian <3003html@gmail.com>
2022-07-19 10:43:52 -0500

    Hash function modified and output chenged

--- Strategy/strategy.go
+++ Strategy/strategy.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	emd5 "crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+)
 
 type passwordProtector struct {
 	user          string
 	passwordName  string
+	password      string
 	hashAlgorithm hashAlgorithm
 }
 
@@ -12,10 +18,11 @@ type hashAlgorithm interface {
 	hash(p *passwordProtector)
 }
 
-func newPasswordProtector(user string, passwordName string, hash hashAlgorithm) *passwordProtector {
+func newPasswordProtector(user string, passwordName string, password string, hash hashAlgorithm) *passwordProtector {
 	return &passwordProtector{
 		user:          user,
 		passwordName:  passwordName,
+		password:      password,
 		hashAlgorithm: hash,
 	}
 }
@@ -24,28 +31,34 @@ func (p *passwordProtector) setHashAlgorith(hash hashAlgorithm) {
 	p.hashAlgorithm = hash
 }
 
-func (p *passwordProtector) hash() {
+func (p *passwordProtector) protect() {
 	p.hashAlgorithm.hash(p)
 }
 
 type sha struct{}
 
 func (sha) hash(p *passwordProtector) {
-	fmt.Printf("Hashing using SHA for %s\n", p.passwordName)
+	h := sha1.New()
+	h.Write([]byte(p.password))
+	sha1Hash := hex.EncodeToString(h.Sum(nil))
+	fmt.Printf("Hashing using SHA for %s: %s\n", p.passwordName, sha1Hash)
 }
 
 type md5 struct{}
 
 func (md5) hash(p *passwordProtector) {
-	fmt.Printf("Hashing using MD5 for %s\n", p.passwordName)
+	h := emd5.New()
+	h.Write([]byte(p.password))
+	md5Hash := hex.EncodeToString(h.Sum(nil))
+	fmt.Printf("Hashing using MD5 for %s: %s\n", p.passwordName, md5Hash)
 }
 
 func main() {
 	sha := &sha{}
 	md5 := &md5{}
 
-	passwordProtector := newPasswordProtector("Christian", "Steam passwords", sha)
-	passwordProtector.hash()
+	passwordProtector := newPasswordProtector("Christian", "Steam password", "1324", sha)
+	passwordProtector.protect()
 	passwordProtector.setHashAlgorith(md5)
-	passwordProtector.hash()
+	passwordProtector.protect()
 }
